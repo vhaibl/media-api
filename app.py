f546f00970f2d635439d5e4b6509ab09c21332c8
vhaibl <44583314+vhaibl@users.noreply.github.com>
2020-10-14 11:41:09 +0300

    Some code refactoring & logger added

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import logging.config
 import os
 import secrets
 import sys
@@ -6,21 +7,21 @@ from http import HTTPStatus
 
 from flask import Flask, jsonify, request, send_from_directory
 
+import media_logging
+
+logging.config.dictConfig(media_logging.LOGGING)
+logger = logging.getLogger('api.upload')
+
 secret = secrets.token_urlsafe(32)
+
 UPLOAD_FOLDER = 'files/'
-EXTERNAL_PATH = 'media\\images\\'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+EXTERNAL_PATH = 'media\\'
 
 app = Flask(__name__)
 app.base_dir = sys.path[0]
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['EXTERNAL_PATH'] = EXTERNAL_PATH
 app.secret_key = secret
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -30,24 +31,35 @@ def where_missing():
 
 @app.route('/<where>/', methods=['POST'])
 def upload_file(where):
-    if not os.path.exists(os.path.join(sys.path[0], app.config['UPLOAD_FOLDER'], where)):
-        os.makedirs(os.path.join(sys.path[0], app.config['UPLOAD_FOLDER'], where))
     if 'photo' in request.files:
-        try:
-            file = request.files['photo']
-            photo_name_file = str(uuid.uuid4()) + f'_{request.files["photo"].filename}'
-            file.save(os.path.join(sys.path[0], app.config['UPLOAD_FOLDER'], where, photo_name_file))
-            return jsonify({'path': os.path.join(app.config['EXTERNAL_PATH'], where, photo_name_file)}), HTTPStatus.OK
-        except Exception as er:
-            # logger.error(er)
-            return jsonify(er), HTTPStatus.BAD_REQUEST
-    return jsonify({'result': 'Image file has not sent'}), HTTPStatus.BAD_REQUEST
+        file_type = 'photo'
+        folder = 'images'
+        return save_file(where, file_type, folder)
+    if 'audio' in request.files:
+        file_type = 'audio'
+        folder = 'images'
+        return save_file(where, file_type, folder)
+
+    return jsonify({'result': 'Media file has not sent'}), HTTPStatus.BAD_REQUEST
 
 
-@app.route('/media/images/<where>/<file>', methods=['GET', 'POST'])
-def serve(where, file):
-    print(os.path.join(app.config['UPLOAD_FOLDER'], where, file))
-    return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER'], where), file)
+def save_file(where, file_type, folder):
+    if not os.path.exists(os.path.join(sys.path[0], app.config['UPLOAD_FOLDER'], folder, where)):
+        os.makedirs(os.path.join(sys.path[0], app.config['UPLOAD_FOLDER'], folder, where))
+    try:
+        file = request.files[file_type]
+        photo_name_file = str(uuid.uuid4()) + f'_{request.files[file_type].filename}'
+        file.save(os.path.join(sys.path[0], app.config['UPLOAD_FOLDER'], folder, where, photo_name_file))
+        return jsonify(
+            {'path': os.path.join(app.config['EXTERNAL_PATH'], folder, where, photo_name_file)}), HTTPStatus.OK
+    except Exception as er:
+        logger.error(er)
+        return jsonify(er), HTTPStatus.BAD_REQUEST
+
+
+@app.route('/media/<file_type>/<where>/<file>', methods=['GET', 'POST'])
+def serve(where, file, file_type):
+    return send_from_directory(os.path.join(app.config['UPLOAD_FOLDER'], file_type, where), file)
 
 
 if __name__ == '__main__':
